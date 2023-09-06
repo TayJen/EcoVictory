276d31ac2f3442ee295c41d72a20246452d9d1af
alex <you@example.com>
2023-09-06 10:43:56 +0300

    intermediate commit

--- Responses.py
+++ Responses.py
@@ -6,16 +6,16 @@ def sample_responses(input_text):
     user_message = str(input_text).lower()
 
     if user_message in ('hello', 'hi', 'sup', 'привет', 'здравствуй'):
-        return "Привет! Просто скинь мне фото состава"
+        return "Привет! Просто скинь мне фото состава продуктов!"
 
     if user_message in ('who are you', 'who are you?', 'кто ты', 'кто ты?'):
-        return 'Я BuildYourTripBot, помогаю с определением достопримечательности и краткой историей'
+        return 'Я EcoVictoryBot, помогаю с определением безопасности продуктов по их составу.  Даю рекомендации по питанию'
 
     return "Я не понимаю тебя :("
 
 
 def photo_response(pred):
-    df = pd.read_csv('data/Описание достопримечательностей.csv')
+    df = pd.read_csv('data/Описание .csv')
     with open('data/labels.json', 'r') as fp:
         labels = json.load(fp)
         pred = labels[str(pred)]
